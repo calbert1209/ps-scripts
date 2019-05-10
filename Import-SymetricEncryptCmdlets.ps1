f3f716ae3ad1f5e0ad21350017bb38e69c545e32
calbert1209 <c_albert@yahoo.com>
2019-05-10 12:06:28 +0900

    add Out-EncryptedFile cmdlet and test function

--- Import-SymetricEncryptCmdlets.ps1
+++ Import-SymetricEncryptCmdlets.ps1
@@ -91,3 +91,53 @@ function Get-EncryptedContent {
       [Runtime.InteropServices.Marshal]::PtrToStringAuto($bstr)
     }
 }
+
+function Out-EncryptedFile {
+  param(
+    # path for created file
+    [Parameter(Mandatory, Position=0)]
+    [String]
+    $Path,
+  
+    # key in byte array form
+    [Parameter(Mandatory, Position=1)]
+    [Byte[]]
+    $Key,
+
+    # encrypted string
+    [Parameter(Mandatory, Position=2, ValueFromPipeline=$true)]
+    [String]
+    $Data
+  )
+
+  Begin {
+    $collection = @()
+  }
+  Process {
+    $collection += $Data
+  }
+  End {
+    $lines = New-Object System.Text.StringBuilder
+    $collectionLength = $collection.Length
+    for ($i = 0; $i -lt $collectionLength; $i++) {
+      $lines.Append($collection[$i]) | Out-Null
+      if ($i -lt ($collectionLength - 1)) {
+        $lines.Append("`n") | Out-Null
+      }
+    }
+    
+    $chars = $lines.ToString().ToCharArray()
+    $secureStr = New-Object System.Security.SecureString
+    $chars | ForEach-Object { $secureStr.AppendChar($_) }
+    ConvertFrom-SecureString -SecureString $secureStr -Key $Key `
+      | Out-File -FilePath $Path -Encoding utf8
+  }
+}
+
+function Test-Idea {
+  $testFile = "C:\users\albert\Desktop\test.txt"
+  $key = Get-KeyBytes -KeyString "mi6"
+  $array = "one", "two", "three"
+  $array | Out-EncryptedFile -Path $testFile -Key $key
+  Get-EncryptedContent -Path $testFile -Key $key
+}
