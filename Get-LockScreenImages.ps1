2a52da25ef9d2e9a475a404068693ae278b53b45
calbert1209 <c_albert@yahoo.com>
2019-09-21 22:36:03 +0900

    remove lock-screen getter script, available as separate project

--- Get-LockScreenImages.ps1
+++
@@ -1,8 +0,0 @@
-$regItemPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Lock Screen\Creative'
-$regItemProps = Get-ItemProperty -Path $regItemPath
-$hotspotImageFolderPath = $regItemProps.HotspotImageFolderPath
-$hotspotImages = Get-ChildItem -Path $hotspotImageFolderPath
-
-# TODO copy to work dir
-# TODO truncate name & add jpg extension
-# TODO filter out non-landscapes & icons
