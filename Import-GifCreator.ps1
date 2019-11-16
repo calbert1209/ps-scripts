5b6a7bdc8e9b16de0c7ed8b9f623f31f726752b1
calbert1209 <c_albert@yahoo.com>
2019-11-16 23:15:15 +0900

    add gif-creator function

---
+++ Import-GifCreator.ps1
@@ -0,0 +1,46 @@
+. ./Import-FfmpegImageUtilities.ps1
+function ConvertTo-Gif {
+  param(
+    # absolute path to file
+    [Parameter(Mandatory = $true)]
+    [String]
+    $FilePath,
+
+    # image id of ffmpeg container
+    # defaults to jrottenberg/ffmpeg:4.1-scratch
+    [Parameter(Mandatory = $false)]
+    [String]
+    $ImageID = "7695c674109a",
+
+    # width of output GIF (scales according to aspect ratio)
+    [Parameter(Mandatory = $false)]
+    [Int16]
+    $Width = 320,
+
+    # frame rate
+    [Parameter(Mandatory = $false)]
+    [Int16]
+    $FrameRate = 10,
+
+    #infinite loop switch
+    [Parameter(Mandatory = $false)]
+    [switch]
+    $NoLoop
+  )
+
+  $file = Get-ChildItem -Path $FilePath
+  $name = $file.Name
+  $basename = $file.BaseName
+  $directoryPath = $file.Directory.FullName
+
+  
+  $inputArg = "-i `"tmp/$name`""
+  $optionsArg = "-vf `"fps=$FrameRate,scale=$($Width):-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse`""
+  $loopArg = "-loop 0"
+  if ($NoLoop) {
+    $loopArg = "";
+  }
+  $outputArg = "`"tmp/$basename.gif`""
+
+  Invoke-FfmpegImage -ImageID $ImageID -WorkingDir $directoryPath -Arguments @($inputArg, $optionsArg, $loopArg, $outputArg)
+}
